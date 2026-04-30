d14833634e37602686d3d009f5172f3be7b1c262
YousufNaveedKhan <yk033127@gmail.com>
2026-04-30 19:35:29 +0500

    Update: Lec02 Cancelled

--- lec02-models_efcore_db/lec02-models_efcore_db/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace lec02_models_efcore_db.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
